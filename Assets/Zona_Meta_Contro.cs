ce40d4073a71de1892dc0d6e8ef528ba3e0f3544
Natter21 <142517692+Natter21@users.noreply.github.com>
2025-10-17 23:37:24 -0300

    detalles

--- Assets/Zona_Meta_Contro.cs
+++ Assets/Zona_Meta_Contro.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Zona_Meta_Contro : MonoBehaviour
 {
     // jugador en la meta
+    public AudioClip sonidoDeLaMeta; // Clip de audio para la meta
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))  
         {
+
+            Debug.Log("¡HAZ LLEGADO A LA META!");
             
-            Debug.Log("¡Meta alcanzada!");
+            if (sonidoDeLaMeta != null)
+                AudioSource.PlayClipAtPoint(sonidoDeLaMeta, transform.position, 1f);
             
         }
     }
